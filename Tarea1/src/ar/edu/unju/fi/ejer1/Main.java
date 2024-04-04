41cab636d5ac99ac0cd17d5807d38b6a5f8c7766
“Rafael Mamani <rafariemi222@gmail.com>
2024-04-04 16:00:20 -0300

    Commit del ejercicio 1.

---
+++ Tarea1/src/ar/edu/unju/fi/ejer1/Main.java
@@ -0,0 +1,17 @@
+package ar.edu.unju.fi.ejer1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float n1=6;
+		float n2=10;
+		float n3=2;
+		float n4=7;
+		float n5=12;
+		float promedio = (n1 + n2 + n3 + n4 + n5)/5;
+		System.out.println(n1 + " " + n2+ " " + n3+ " " + n4+ " " + n5);
+		System.out.println(promedio);
+	}
+
+}
