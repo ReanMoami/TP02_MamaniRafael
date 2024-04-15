9ae02a764fb1121e06a4c0ed4fcad2f2195ae4e0
“Rafael <rafariemi222@gmail.com>
2024-04-15 19:53:14 -0300

    Ejercicio 2.

---
+++ Tarea1/src/ar/edu/unju/fi/ejer2/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejer2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre_p = "Fracia" ;
+		int edad_per = 18;
+		double alt_edi = 20.45;
+		double produc_super = 4567.50 ;
+		String telefono = "+54 388 460 3736";
+		double cosMedio = Math.cos(0.5);
+		System.out.println("Nombre del país: " + nombre_p);
+        System.out.println("Edad de la persona: " + edad_per);
+        System.out.println("Altura del edificio: " + alt_edi + " metros");
+        System.out.println("Precio del producto: " + produc_super + " yenes");
+        System.out.println("Número de teléfono: " + telefono);
+        System.out.println("Coseno de 0.5: " + cosMedio);
+	}
+}
