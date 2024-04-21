2ea227b38d0777355e377d11a596925e412e11fc
“Rafael <rafariemi222@gmail.com>
2024-04-20 23:35:55 -0300

    Ejercicio 11.

---
+++ Tarea1/src/ar/edu/unju/fi/ejer11/Main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejer11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int j = 40,value;
+        for (int i = 2; j > -1; i += 1) {
+            value = i * j;
+            System.out.print(value + "  ");
+            j -= 1;        }
+	}
+
+}
